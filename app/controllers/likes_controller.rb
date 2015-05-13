1386b4abbe84d2c6b67925fd788c9ae8fb927b67
Simon Kotwicz <simonkotwicz@gmail.com>
2015-05-13 00:09:25 -0400

    worked on likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_likeable
-  before_action :set_like
+  before_action :set_likeable, only: [:create]
+  before_action :set_like, only: [:destroy]
   before_action :restrict
 
   def create
@@ -54,7 +54,7 @@ class LikesController < ApplicationController
 
     def restrict
       if @likeable.liked_by(current_user)
-        redirect_to @question
+
       end
     end
 
