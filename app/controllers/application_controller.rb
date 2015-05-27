5ab596e6a8374f644176ac7e36b4275389bac7a9
Ahmad <wisamfaithful@gmail.com>
2015-05-27 08:49:36 -0400

    user first and last names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:university]
-    devise_parameter_sanitizer.for(:account_update) << [:university]
+    devise_parameter_sanitizer.for(:account_update) << [:university, :first_name, :last_name]
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527111322) do
+ActiveRecord::Schema.define(version: 20150527124029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,8 @@ ActiveRecord::Schema.define(version: 20150527111322) do
     t.datetime "updated_at"
     t.string   "university"
     t.integer  "score",                  default: 0
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
