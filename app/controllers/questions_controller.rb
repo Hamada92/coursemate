5953b1b681592b43556a39bcdb93c5e746584c8d
Ahmad <wisamfaithful@gmail.com>
2015-05-28 17:53:11 -0400

    index query optimization

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
 
   def index
     if user_signed_in?
-      @questions = Question.tagged_with_university current_user.university
+      @questions = Question.tagged_with_university(current_user.university).includes(:answers, :likes, :tags)
       @tags = Tag.with_university current_user.university
     else
       @questions = Question.all
