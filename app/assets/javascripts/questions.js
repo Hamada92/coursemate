c2dc3c5f1d0c458d9930c4b85222b355723576ea
Simon Kotwicz <simonkotwicz@gmail.com>
2015-06-07 21:17:36 -0400

    refactoring

---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,35 @@
+$(document).ready(ready)
+$(document).on('page:load', ready)
+
+function ready() {
+
+  if ($("#category_select").val() == "Program Related") 
+    $("#tag_input").attr("placeholder", "Enter a program... eg. Electrical Engineering");
+  else if ($("#category_select").val() == "University Related") 
+    $("#tag_input").hide();
+
+  $("#category_select").change(function () {
+    if (this.value == "University Related") {
+      $("#tag_input").hide();
+      $("#tag_input").val("General");
+    }
+    else {
+      $("#tag_input").show();
+      if (this.value == "Course Related") {
+        $("#tag_input").val("");
+        $("#tag_input").attr("placeholder", "Enter a course name... eg. MATH 101");
+      }
+      else if (this.value == "Program Related") {
+        $("#tag_input").val("");
+        $("#tag_input").attr("placeholder", "Enter a program... eg. Electrical Engineering");
+      }
+    }
+  });
+
+  $("#question_body").keyup(function() {
+    $('.question_keydown').html($('#question_body').val());
+  });
+
+}
+
+
