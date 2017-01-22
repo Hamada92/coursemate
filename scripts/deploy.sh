89661e76c94273541d35ad859d67745748655582
Simon Kotwicz <simonkotwicz@gmail.com>
2017-01-22 18:06:51 -0500

    add logo (#55)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git remote add dokku ${DEPLOY_REPO_URI}
+git remote add dokku ${DEPLOY_REPO_URI}:${DOMAIN}
 git push dokku master:master
+ssh ${DEPLOY_REPO_URI} run ${DOMAIN} rake db:migrate
