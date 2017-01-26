39b8ab8eec5c7549bbe0c672ee2281229ff2610f
Simon Kotwicz <simonkotwicz@gmail.com>
2017-01-26 16:01:20 -0500

    set host in default_url_options for prod (#58)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.asset_host = 'https://' + ENV['DOMAIN']
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
