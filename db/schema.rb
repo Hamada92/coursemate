dd73e0acb1488b23a5b9ad6c89bd736c9baa0bab
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-06-03 12:45:32 -0400

    universities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602173250) do
+ActiveRecord::Schema.define(version: 20170603144915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(version: 20170602173250) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                               null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -162,7 +162,6 @@ ActiveRecord::Schema.define(version: 20170602173250) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "university"
     t.integer  "score",                  default: 0
     t.string   "first_name"
     t.string   "last_name"
@@ -187,10 +186,10 @@ ActiveRecord::Schema.define(version: 20170602173250) do
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.text     "about_me"
+    t.text     "university_domain"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["university"], name: "index_users_on_university", using: :btree
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
     t.index ["username"], name: "index_users_on_username", using: :btree
   end
@@ -204,4 +203,5 @@ ActiveRecord::Schema.define(version: 20170602173250) do
   add_foreign_key "questions", "users"
   add_foreign_key "taggings", "questions"
   add_foreign_key "taggings", "tags"
+  add_foreign_key "users", "universities", column: "university_domain", primary_key: "domain", name: "users_university_domain_fkey"
 end
