0062b4f07dd1010cfbd52828ea75795eda45b10b
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-06-03 14:14:48 -0400

    universities

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,4 +1,5 @@
 class University < ApplicationRecord
   self.primary_key = 'domain'
   has_many :courses, foreign_key: 'university_domain', dependent: :destroy
+  has_many :users, foreign_key: 'university_domain', dependent: :destroy
 end
