b3ba206207564ad173d676552c52eea4681941f9
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-07-23 18:44:44 -0400

    restrict question deletion

---
+++ db/migrate/20170723212641_modify_answers_question_id.rb
@@ -0,0 +1,11 @@
+class ModifyAnswersQuestionId < ActiveRecord::Migration[5.0]
+  def up
+    execute <<-SQL
+      begin;
+        alter table answers drop constraint fk_rails_3d5ed4418f;
+
+        alter table answers add constraint question_id_fkey
+          foreign key (question_id) references questions(id) on delete restrict;
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723141041) do
+ActiveRecord::Schema.define(version: 20170723212641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 20170723141041) do
     t.text     "name",              null: false
     t.text     "university_domain", null: false
     t.datetime "created_at"
-    t.index ["name", "university_domain"], name: "name_and_univresities_domain", using: :btree
     t.index ["university_domain"], name: "univresities_domain", using: :btree
   end
 
@@ -54,7 +53,7 @@ ActiveRecord::Schema.define(version: 20170723141041) do
     t.index ["group_id", "user_id"], name: "group_enrollments_group_id_user_id", using: :btree
   end
 
-  create_table "groups", force: :cascade do |t|
+  create_table "groups", id: :integer, default: -> { "nextval('groups_id_seq1'::regclass)" }, force: :cascade do |t|
     t.text     "university_domain",            null: false
     t.text     "course_name",                  null: false
     t.integer  "creator_id"
@@ -87,7 +86,7 @@ ActiveRecord::Schema.define(version: 20170723141041) do
     t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
-  create_table "notifications", force: :cascade do |t|
+  create_table "notifications", id: :integer, default: -> { "nextval('notifications_id_seq1'::regclass)" }, force: :cascade do |t|
     t.integer "comment_id"
     t.integer "answer_id"
     t.integer "like_id"
@@ -166,7 +165,7 @@ ActiveRecord::Schema.define(version: 20170723141041) do
     t.index ["username"], name: "index_users_on_username", using: :btree
   end
 
-  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "questions", name: "question_id_fkey", on_delete: :restrict
   add_foreign_key "answers", "users"
   add_foreign_key "comments", "answers", name: "comments_answer_id_fkey"
   add_foreign_key "comments", "groups", name: "comments_group_id_fkey"
@@ -174,21 +173,11 @@ ActiveRecord::Schema.define(version: 20170723141041) do
   add_foreign_key "courses", "universities", column: "university_domain", primary_key: "domain", name: "courses_university_domain_fkey"
   add_foreign_key "group_enrollments", "groups", name: "group_enrollments_group_id_fkey"
   add_foreign_key "group_enrollments", "users", name: "group_enrollments_user_id_fkey"
-  add_foreign_key "groups", "courses", column: "course_name", primary_key: "name", name: "groups_course_name_fkey", on_delete: :cascade
-  add_foreign_key "groups", "universities", column: "university_domain", primary_key: "domain", name: "groups_university_domain_fkey"
-  add_foreign_key "groups", "users", column: "creator_id", name: "groups_creator_id_fkey"
   add_foreign_key "likes", "answers", name: "likes_answer_id_fkey"
   add_foreign_key "likes", "questions", name: "likes_question_id_fkey"
-  add_foreign_key "likes", "users"
-  add_foreign_key "notifications", "answers", name: "notifications_answer_id_fkey"
-  add_foreign_key "notifications", "comments", name: "notifications_comment_id_fkey"
-  add_foreign_key "notifications", "likes", name: "notifications_like_id_fkey"
-  add_foreign_key "notifications", "users", name: "notifications_user_id_fkey"
   add_foreign_key "question_html_bodies", "questions", name: "question_html_bodies_question_id_fkey"
   add_foreign_key "questions", "courses", column: "course_name", primary_key: "name", name: "questions_course_name_fkey", on_delete: :cascade
   add_foreign_key "questions", "universities", column: "university_domain", primary_key: "domain", name: "questions_university_domain_fkey"
-  add_foreign_key "questions", "users"
-  add_foreign_key "users", "universities", column: "university_domain", primary_key: "domain", name: "users_university_domain_fkey"
 
   create_view "group_indices",  sql_definition: <<-SQL
       SELECT groups.id,
