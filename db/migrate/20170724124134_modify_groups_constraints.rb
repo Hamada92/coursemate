1615ed02783079a0708c7615f86296ba962e3576
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-07-24 10:32:18 -0400

    table constraint to skip checking old rows

--- db/migrate/20170724124134_modify_groups_constraints.rb
+++ db/migrate/20170724124134_modify_groups_constraints.rb
@@ -7,7 +7,7 @@ class ModifyGroupsConstraints < ActiveRecord::Migration[5.0]
       alter table groups drop constraint IF EXISTS day_at_least_today;
       alter table groups drop constraint IF EXISTS day_at_leat_today;
       alter table groups drop constraint IF EXISTS end_time_greater_than_start_time;
-      alter table groups add constraint starts_at_is_today_or_later CHECK(starts_at AT TIME ZONE 'UTC' >= current_timestamp)
+      alter table groups ADD CHECK(starts_at AT TIME ZONE 'UTC' >= current_timestamp) NOT VALID
     SQL
 
     create_view :group_shows
