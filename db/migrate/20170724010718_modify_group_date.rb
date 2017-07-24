4551978ef42f845e1880679e7261985e2560d1ce
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-07-24 10:10:04 -0400

    migration

--- db/migrate/20170724010718_modify_group_date.rb
+++ db/migrate/20170724010718_modify_group_date.rb
@@ -3,6 +3,7 @@ class ModifyGroupDate < ActiveRecord::Migration[5.0]
     drop_view :group_shows
     drop_view :group_indices
     execute <<-SQL
+      alter table groups drop constraint IF EXISTS day_at_leat_today;
       alter table groups alter column day TYPE timestamp with time zone;
     SQL
     create_view :group_shows
