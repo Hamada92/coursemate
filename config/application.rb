fd528fd8ace286b6c8fdeb4883b4cc55ae07b767
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-07-29 18:28:41 -0400

    structure sql

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module AnswerMe
       enable_starttls_auto: true
     }
 
+    #load schema from structure.sql
+    config.active_record.schema_format = :sql
+
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
