342fefd5bd5440440274112cac9ab4381d2851bf
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-08-14 18:14:48 -0400

    cascade groups

---
+++ db/migrate/20170814221149_add_more_cascades.rb
@@ -0,0 +1,15 @@
+class AddMoreCascades < ActiveRecord::Migration[5.0]
+  def up
+    execute <<-SQL
+      begin;
+        alter table group_enrollments drop constraint IF EXISTS group_enrollments_group_id_fkey;
+        alter table group_enrollments add constraint group_enrollments_group_id_fkey
+          foreign key (group_id) references groups(id) on delete cascade;
+
+        alter table group_enrollments drop constraint IF EXISTS group_enrollments_user_id_fkey;
+        alter table group_enrollments add constraint group_enrollments_user_id_fkey
+          foreign key (user_id) references users(id) on delete cascade;
+
+    SQL
+  end
+end
