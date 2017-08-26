15008d5064cbc36fcb8752cf2f738c3d21d99698
Ahmad Al-kheat <ahmad1999a@yahoo.com>
2017-08-26 11:06:34 -0400

    dont use the view in the controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,5 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_notification, only: [:mark_read]
 
   def index
     @notifications = current_user.notification_lists.paginate(per_page:10, page: params[:page])
@@ -14,20 +13,18 @@ class NotificationsController < ApplicationController
   end
 
   def mark_read
-    if @notification_list.notification_type == 'comment'
-      CommentStatus.find_by(comment_id: @notification_list.notifier_id, user_id: current_user.id).update_column(:seen, true)
-    elsif @notification_list.notification_type == 'answer'
-      Answer.find(@notification_list.notifier_id).update_column(:seen, true)
-    elsif @notification_list.notification_type == 'like'
-      Like.find(@notification_list.notifier_id).update_column(:seen, true)
+    case params[:type]
+    when 'comment'
+      CommentStatus.find_by(comment_id: params[:id], user_id: current_user.id).update_column(:seen, true)
+    when 'like'
+      Like.find(params[:id]).update_column(:seen, true)
+    when 'answer'
+      Answer.find(params[:id]).update_column(:seen, true)
     end
     head :no_content
   end
 
-  private
-
-    def set_notification
-      @notification_list = NotificationList.find_by(notifier_id: params[:id], notified_user: current_user.id)
-    end
-
+  def authorize
+    #to do
+  end
 end
