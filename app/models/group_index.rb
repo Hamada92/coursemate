b2a1ee057bfc473084b56614f3234419c1c47c3a
Ahmad <ahmad.kaate@gmail.com>
2017-09-10 15:52:56 -0400

    sort groups

--- app/models/group_index.rb
+++ app/models/group_index.rb
@@ -2,7 +2,7 @@ class GroupIndex < ApplicationRecord
   include GroupMethods
   self.primary_key = 'id'
 
-  default_scope { order(status: :asc, id: :desc) }      
+  default_scope { order(id: :desc) }      
 
   belongs_to :creator, class_name: 'User'
 
