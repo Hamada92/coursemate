91dcf990190ad8c013494d74aa1eaf88af5b66e9
Ahmad Al-kheat <ahmad@himama.com>
2018-03-16 10:21:28 -0400

    order by status and id

--- app/models/group_index.rb
+++ app/models/group_index.rb
@@ -2,7 +2,7 @@ class GroupIndex < ApplicationRecord
   include GroupMethods
   self.primary_key = 'id'
 
-  default_scope { order("CASE status WHEN 'active' THEN 1 ELSE 2 END") }      
+  default_scope { order("CASE status WHEN 'active' THEN 1 ELSE 2 END ASC, id DESC") }      
 
   belongs_to :creator, class_name: 'User'
 
--- test/models/group_index_test.rb
+++ test/models/group_index_test.rb
@@ -6,12 +6,24 @@ class GroupIndexTest < ActiveSupport::TestCase
     create(:course)
     @group = create(:group)
     @group_2 = create(:group)
+    @group_3 = create(:group, status: 'completed')
+    @group_4 = create(:group, status: 'cancelled')
     create_list(:group_enrollment, 3, group: @group)
     create_list(:group_enrollment, 2, group: @group_2)
   end
 
   test 'returns the right number of groups' do 
-    assert_equal 2, GroupIndex.count
+    assert_equal 4, GroupIndex.count
     assert_equal @group_2.id, GroupIndex.first.id
   end
+
+  test 'orders by the right columns' do 
+    @group_2.cancel!
+
+    groups = GroupIndex.all
+
+    assert_equal [@group.id, @group_4.id, @group_3.id, @group_2.id], groups.pluck(:id)
+
+  end
+
 end
