6bba03cc7d948d962efbfad3ddffbd77683c7903
Ahmad Al-kheat <ahmad@himama.com>
2018-03-16 10:55:13 -0400

    question edit error messages

--- app/models/group_index.rb
+++ app/models/group_index.rb
@@ -2,6 +2,7 @@ class GroupIndex < ApplicationRecord
   include GroupMethods
   self.primary_key = 'id'
 
+  # active groups first ordered by id, then completed and cancelled ordererd by id
   default_scope { order("CASE status WHEN 'active' THEN 1 ELSE 2 END ASC, id DESC") }      
 
   belongs_to :creator, class_name: 'User'
