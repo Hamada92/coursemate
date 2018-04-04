df79569086fd7c9eab8be7b6b145413786c12f58
Ahmad Al-kheat <ahmad@himama.com>
2018-04-04 11:00:33 -0400

    materialized view

--- app/models/group_index.rb
+++ app/models/group_index.rb
@@ -1,6 +1,7 @@
 class GroupIndex < ApplicationRecord
   include GroupMethods
   self.primary_key = 'id'
+  self.table_name = "group_indices_unmaterialized"
 
   # active groups first ordered by id, then completed and cancelled ordererd by id
   default_scope { order("CASE status WHEN 'active' THEN 1 ELSE 2 END ASC, id DESC") }      
---
+++ db/migrate/20180404145230_rename_group_indices_view.rb
@@ -0,0 +1,7 @@
+class RenameGroupIndicesView < ActiveRecord::Migration[5.0]
+  def change
+    execute <<-sql
+      ALTER VIEW group_indices RENAME TO group_indices_unmaterialized;
+    sql
+  end
+end
